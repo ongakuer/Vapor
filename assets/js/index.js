319061ee493b14003ab8cf095e02c2ced55bf6d7
Relex <relex@skyplatanus.com>
2014-06-24 22:51:24 +0800

    code style

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,10 +4,9 @@
 
 /*globals jQuery, document */
 (function ($) {
-    "use strict";
+    //"use strict";
 
-    $(document).ready(function(){
-
+    $(document).ready(function(){    		
         // On the home page, move the blog icon inside the header 
         // for better relative/absolute positioning.
 
